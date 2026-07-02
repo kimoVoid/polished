c3583e680d9af53d73a3fc18223cdd40305cfb9e
kimoVoid <bordakmc@gmail.com>
2026-07-02 16:28:19 -0300

    Lowkey forgot the language key

--- src/main/java/io/github/kimovoid/polished/mixin/client/texturepacksbutton/OptionsScreenMixin.java
+++ src/main/java/io/github/kimovoid/polished/mixin/client/texturepacksbutton/OptionsScreenMixin.java
@@ -1,5 +1,7 @@
 package io.github.kimovoid.polished.mixin.client.texturepacksbutton;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.kimovoid.polished.client.PolishedClient;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
@@ -10,7 +12,6 @@ import net.minecraft.locale.Language;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
@@ -18,18 +19,18 @@ import java.util.List;
 @Mixin(OptionsScreen.class)
 public abstract class OptionsScreenMixin extends Screen {
 
-    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 2))
-    private boolean addTexturePacks(List<ButtonWidget> list, Object obj) {
+    @WrapOperation(method = "init", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 2))
+    private boolean addTexturePacks(List<Object> instance, Object obj, Operation<Boolean> original) {
         if (PolishedClient.CONFIG.texturePacksButton.get()) {
             Language lang = Language.getInstance();
             boolean sc = FabricLoader.getInstance().isModLoaded("sound-categories");
 
-            list.add(new ButtonWidget(99, this.width / 2 + (sc ? 5 : -100), this.height / 6 + (sc ? 0 : 76 + 12), sc ? 150 : 200, 20, lang.translate("options.texturePacks")));
-            list.add(sc ? (ButtonWidget) obj : new ButtonWidget(101, this.width / 2 - 100, this.height / 6 + 98 + 12, lang.translate("options.video")));
+            original.call(instance, new ButtonWidget(99, this.width / 2 + (sc ? 5 : -100), this.height / 6 + (sc ? 0 : 76 + 12), sc ? 150 : 200, 20, lang.translate("options.texturePacks")));
+            original.call(instance, sc ? obj : new ButtonWidget(101, this.width / 2 - 100, this.height / 6 + 98 + 12, lang.translate("options.video")));
             return true;
         }
 
-        return list.add((ButtonWidget) obj);
+        return original.call(instance, obj);
     }
 
     @Inject(method = "buttonClicked", at = @At("HEAD"))
