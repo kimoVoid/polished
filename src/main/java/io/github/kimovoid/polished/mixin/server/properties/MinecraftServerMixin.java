811e5f8085dd15672a2653a5ce2332a59f8164fa
kimoVoid <bordakmc@gmail.com>
2026-08-01 15:37:24 -0300

    Pre-generate nether option for servers

--- src/main/java/io/github/kimovoid/polished/mixin/server/properties/MinecraftServerMixin.java
+++ src/main/java/io/github/kimovoid/polished/mixin/server/properties/MinecraftServerMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.kimovoid.polished.server.PolishedServer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.ServerProperties;
 import net.minecraft.server.world.ServerWorld;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,5 +23,17 @@ public class MinecraftServerMixin {
     )
     public void setDifficulty(ServerWorld instance, int value, Operation<Void> original) {
         original.call(instance, PolishedServer.INSTANCE.properties.difficulty);
+    }
+
+    @WrapOperation(
+            method = "loadWorld",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/ServerProperties;getBoolean(Ljava/lang/String;Z)Z",
+                    ordinal = 2
+            )
+    )
+    private boolean setPreGenerateNether(ServerProperties instance, String key, boolean defaultValue, Operation<Boolean> original) {
+        return original.call(instance, key, defaultValue) && PolishedServer.INSTANCE.properties.preGenerateNether;
     }
 }
--- src/main/java/io/github/kimovoid/polished/server/PolishedServerProperties.java
+++ src/main/java/io/github/kimovoid/polished/server/PolishedServerProperties.java
@@ -6,6 +6,7 @@ public class PolishedServerProperties {
 
     public String serverMotd;
     public boolean deathCoordinates;
+    public boolean preGenerateNether;
     public int spawnProtection;
     public int difficulty;
     public int playerSleepingPercentage;
@@ -13,6 +14,7 @@ public class PolishedServerProperties {
     public PolishedServerProperties(MinecraftServer server) {
         this.serverMotd = server.properties.getString("motd", "A Minecraft Server");
         this.deathCoordinates = server.properties.getBoolean("death-coordinates", false);
+        this.preGenerateNether = server.properties.getBoolean("pre-generate-nether", false);
         this.spawnProtection = server.properties.getInt("spawn-protection", 16);
         this.difficulty = server.properties.getInt("difficulty", 1);
         this.playerSleepingPercentage = server.properties.getInt("player-sleeping-percentage", 100);
